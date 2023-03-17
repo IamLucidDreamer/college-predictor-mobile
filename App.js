08a2ab64b27493bfeab66c2a87989c395dc08608
Manas <shuklamanasofficial@gmail.com>
2023-03-18 01:52:01 +0530

    Feat : App JS file

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as Status } from 'expo-status-bar';
+import { View, BackHandler, Platform, StatusBar, SafeAreaView, ActivityIndicator } from 'react-native';
+import store from "./src/store/store"
+import { Provider } from 'react-redux';
+import Navigation from './src/navigation/Navigation';
+import theme from './src/helpers/theme';
+import Toast from 'react-native-toast-message';
+import WebView from 'react-native-webview';
+import { useEffect, useRef, useState } from 'react';
+import NewUpdatePage from './src/UpdateScreen';
+import { server, serverUnauth } from './src/helpers/apiCall';
+import { expo } from './app.json'
 
 export default function App() {
+  const [updateData, setUpdateData] = useState()
+  useEffect(() => {
+    updateChecker()
+  }, [])
+
+  const updateChecker = () => {
+    server.get('/update')
+      .then((res) => { setUpdateData(res.data) })
+      .catch((err) => { console.log(err); })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Status style="dark" />
+
+      {updateData?.version > expo?.android?.versionCode ?
+        <NewUpdatePage updateUrl={updateData?.updateUrl} />
+        :
+        <>
+          <Navigation />
+          <Toast />
+        </>}
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
