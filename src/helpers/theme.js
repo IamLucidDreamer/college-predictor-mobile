e71d14646a5911d57c7963732857438cc0647ccf
Manas <shuklamanasofficial@gmail.com>
2023-04-29 07:47:39 +0530

    Feat : Theme File Added

---
+++ src/helpers/theme.js
@@ -0,0 +1,13 @@
+const colors = {
+  primary: "#60DD04",
+  secondary: "#515151",
+  secondaryTransparent: "rgba(81, 81, 81, 0.20)",
+  white: "#ffffff"
+};
+
+const fontSize = {
+  heading: "35",
+  paragraph: "18",
+};
+
+export default { colors, fontSize };
